258afd8211869ac9c5392cc55b69ecbbcf9e036e
Luiz Ricardo <luizrrp@gmail.com>
2015-09-18 13:42:16 -0300

    add inputs group

--- searchform.php
+++ searchform.php
@@ -8,9 +8,10 @@
 ?>
 
 <form method="get" id="searchform" class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<div class="form-group">
-		<label for="s" class="sr-only"><?php _e( 'Search', 'odin' ); ?></label>
+	<div class="input-group">
 		<input type="search" class="form-control" name="s" id="s" />
+		<span class="input-group-btn">
+			<input type="submit" class="btn btn-default" value="<?php esc_attr_e( 'Search', 'odin' ); ?>" />
+		</span>
 	</div>
-	<input type="submit" class="btn btn-default" value="<?php esc_attr_e( 'Search', 'odin' ); ?>" />
 </form>
